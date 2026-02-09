dbe078d7499de815ecb894b9ba8f71696d9fa011
Sorin Gore <soringoredev@gmail.com>
2026-02-09 22:59:12 +0200

    Add ex

---
+++ ex_nou.py
@@ -0,0 +1,12 @@
+'''
+
+
+DARIANA!!!!
+
+CReeaza un exercitiu nou in care s aavem doua variabile
+username si password..
+Utilizatorul trebuie s apoatya sa le introduca de la tastatura.
+
+si tu sa le afisezi la ecran
+
+'''
