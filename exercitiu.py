bd04322b825314096ce4c2f6d00d80bc7bf0bea7
Sorin Gore <soringoredev@gmail.com>
2026-02-09 22:48:47 +0200

    Add task-1

---
+++ exercitiu.py
@@ -0,0 +1,10 @@
+'''
+Task-1 Dariana
+
+Creeaza un program care accepta introducerea de la tastatura.
+User-ul introduce varsta si programul ii spune daca este adult sau copil.
+Daca e mai mare decat 18 adult, de altfel copil.
+
+
+
+'''
